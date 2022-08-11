62d25b50ac4b6c51bbcd867ddf17d8c40a41dc5b
lihaohao <2495041749@qq.com>
2022-08-11 23:18:05 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup></script>
+
+<template>
+  <div class="main">
+    <div class="main-top"></div>
+    <div class="main-bottom">
+      <div class="main-bottom-detail meta-width" v-for="item in 30"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.main-top {
+  height: 60px;
+  background: black;
+}
+
+.main-bottom {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 0 20px;
+}
+
+.main-bottom-detail {
+  margin: 10px 0;
+  height: 255px;
+  width: 300px;
+  background: #ccc;
+  border-radius: 3px;
+}
+</style>
